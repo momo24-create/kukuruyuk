7c711b996191ad5bc4ed6ec71798d152ba6448d0
momo24-create <mochichi2424@gmail.com>
2026-04-17 13:45:12 +0700

    Update getproxy.py

--- getproxy.py
+++ getproxy.py
@@ -392,7 +392,7 @@ proxy_urls = [
     "https://raw.githubusercontent.com/dpangestuw/Free-Proxy/refs/heads/main/socks5_proxies.txt",
     "https://raw.githubusercontent.com/dpangestuw/Free-Proxy/refs/heads/main/socks4_proxies.txt",
     "https://raw.githubusercontent.com/dpangestuw/Free-Proxy/refs/heads/main/http_proxies.txt",
-    "https://raw.githubusercontent.com/dpangestuw/Free-Proxy/refs/heads/main/allive.txt",
+    "https://raw.githubusercontent.com/dpangestuw/Free-Proxy/refs/heads/main/All_proxies.txt",
     "https://raw.githubusercontent.com/databay-labs/free-proxy-list/refs/heads/master/socks5.txt",
     "https://raw.githubusercontent.com/databay-labs/free-proxy-list/refs/heads/master/http.txt",
     "https://raw.githack.com/zloi-user/hideip.me/main/connect.txt",
